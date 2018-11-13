e49700415811d8e7f113207c0137f83af4d8cc18
Takahiro Yamamoto <tak.guruguru.4@gmail.com>
2018-11-14 03:09:21 +0900

    update 181113

--- common.py
+++ common.py
@@ -1,6 +1,8 @@
 import numpy as np
 import os
-import load_gwdata as load
+import matplotlib.pyplot as plt
+from scipy import optimize
+#import load_gwdata as load
 
 
 def mkdir(dir):
@@ -114,12 +116,91 @@ def noise_inject_ringdown(waveformset, pSNR, length=512, bandpass=False):
 
 
 
+#------------------------------------------------------------
+# Gaussian Fitting (2d)
+#------------------------------------------------------------
 
+def gaussian2d(height, x_mean, y_mean, cov_xx, cov_yy, cov_xy):
+
+    detcov = cov_xx * cov_yy - cov_xy**2.0
+    gam_xx = cov_yy / detcov
+    gam_yy = cov_xx / detcov
+    gam_xy = -cov_xy / detcov
+    #norm = 1.0 / (2.0*np.pi*(abs(detcov)**0.5))
+
+    return lambda x,y: height * np.exp( - (x-x_mean)**2.0*gam_xx/2.0 - (y-y_mean)**2.0*gam_yy/2.0 - 2.0*(x-x_mean)*(y-y_mean)*gam_xy)
+    
+'''
+def moments(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution by calculating its
+    moments 
+    data has the shape (N, 2). N is the number of the data.
+    """
+    
+    total = data.sum()
+    data_norm = data / total
+    N = data.shape[0]
+    
+    mean = np.mean(data_norm, axis=0)
+    cov = np.cov(data_norm.T)
+    
+    height = data.max()
+    
+    print(height, mean[0], mean[1], cov[0,0], cov[1,1], cov[0,1])
+    return height, mean[0], mean[1], cov[0,0], cov[1,1], cov[0,1]
+'''
+
+
+
+def moments(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution by calculating its
+    moments """
+    total = data.sum()
+    X, Y = np.indices(data.shape)
+    x = (X*data).sum()/total
+    y = (Y*data).sum()/total
+    col = data[:, int(y)]
+    width_x = np.sqrt(np.abs((np.arange(col.size)-y)**2*col).sum()/col.sum())
+    row = data[int(x), :]
+    width_y = np.sqrt(np.abs((np.arange(row.size)-x)**2*row).sum()/row.sum())
+    height = data.max()
+    width = 0.0
+    return height, x, y, width_x, width_y, width
+
+
+def fitgaussian(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution found by a fit"""
+    params = moments(data)
+    errorfunction = lambda p: np.ravel(gaussian2d(*p)(*np.indices(data.shape)) - data)
+    p, success = optimize.leastsq(errorfunction, params)
+    return p
 
 
 
 if __name__=='__main__':
-    hp = np.load('TestEOB_hPlus.npy')
-    data = noise_inject(hp, 4.0)
-    print(data.shape, hp.shape)
+    Xin, Yin = np.mgrid[0:201, 0:201]
+    data = gaussian2d(3.0, 100, 100, 40, 50, 0.0)(Xin, Yin) + np.random.random(Xin.shape)
 
+    plt.matshow(data, cmap=plt.cm.gist_earth_r)
+
+    params = fitgaussian(data)
+    fit = gaussian2d(*params)
+
+    plt.contour(fit(*np.indices(data.shape)), cmap=plt.cm.copper)
+    ax = plt.gca()
+    (height, x, y, cov_xx, cov_yy, cov_xy) = params
+
+    plt.text(0.95, 0.05, """
+    x : %.1f
+    y : %.1f
+    cov_xx : %.1f
+    cov_yy : %.1f
+    cov_xy: %.1f""" %(x, y, np.sqrt(cov_xx), np.sqrt(cov_yy), np.sqrt(abs(cov_xy))),
+            fontsize=16, horizontalalignment='right',
+            verticalalignment='bottom', transform=ax.transAxes)
+    
+    plt.savefig('figure.png')
+    
