04e5a09b7844deacea6aee0e1eaeae862a9b6b70
tak.yamamoto <tak.guruguru.4@gmail.com>
2019-02-09 19:40:36 +0900

    update 190208

--- gwmemory/lisa_inspiral.py
+++ gwmemory/lisa_inspiral.py
@@ -1,24 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import tukey
+from scipy.signal import tukey, spectrogram
 from scipy.constants import G, c
 from scipy.special import gamma, fresnel
 
 PI = np.pi
 PI2 = PI*PI
 Msun = 1.9884 * 1e+30
-Mpc = 3e+24
+Mpc = 3e+22
 
-def inspiral(t, f, Mc, amp=1.0, phi0=0.0, dist=1.0):
+def inspiral(t, f0, Mc, phi0=0.0, dist=1.0):
 
     
-    df = (96* PI**(8.0/3.0) / 5) * f**(11.0/3.0) * (Mc*Msun)**(5.0/3.0) * (G/c**3.0)**(5.0/3.0)
-    amp = 5.0 * df / (96 * PI2 * f**3.0 * dist) * c
-    phi = 2*PI*(f+0.5*df*t)*t + phi0
+    df = (96* PI**(8.0/3.0) / 5) * f0**(11.0/3.0) * (G*Mc*Msun/(c**3.0))**(5.0/3.0)
+    amp = 5.0 * df / (96 * PI2 * f0**3.0 * dist * Mpc) * c
+    phi = 2*PI*(f0+0.5*df*t)*t + phi0
     hp = amp * np.cos(phi)
     hc = -amp * np.sin(phi)
 
-    return hp, hc
+    return hp, hc, amp, phi, df
+
+
+def inspiral02(t, f0, df, amp=1.0, phi0=0.0):
+
+    phi = 2*np.pi*(f0+0.5*df*t)*t + phi0
+    hp = amp * np.cos(phi)
+    hc = amp * np.sin(phi)
+
+    return hp, hc, phi
 
 
 def integralsin2(x):
@@ -49,9 +58,28 @@ def inspiral_memory(t, f, Mc, dist=1.0, phi0=0.0):
 
 if __name__ == '__main__':
 
-    time = np.arange(0.0, 1.0, 1.0/4096)
-    hp, hc = inspiral(time, 10.0, 1.0)
+    f0 = 50.0
+    fs = 4096
+    t = np.arange(0.0, 500.0, 1.0/fs)
+    hp, hc, amp, phi, df = inspiral(t, f0, 1.0)
+
+    freq, time, spec = spectrogram(hp, fs=4096, nperseg=10*4096)
 
     plt.figure()
-    plt.plot(time, hp)
+    plt.plot(t, hp)
+    plt.xlabel('time[s]')
+    plt.ylabel('h(t)')
+
+    plt.figure()
+    plt.plot(t[:-1], np.diff(phi)/(2.0*np.pi)*fs)
+    plt.xlabel('time[sec]')
+    plt.ylabel('frequency[Hz]')
+
+    plt.figure()
+    plt.pcolormesh(time, freq, spec, cmap='gray')
+    plt.colorbar()
+    plt.xlabel('time[s]')
+    plt.ylabel('frequency[Hz]')
+
+
     plt.show()
