3b1c7f7d55356a0c34edd29b97083129f485983e
Takahiro Yamamoto <tak.guruguru.4@gmail.com>
2019-03-21 17:43:10 +0900

    update 190321

--- ppplot.py
+++ ppplot.py
@@ -1,6 +1,11 @@
 import numpy as np
 from scipy import stats
-import matplotlib.pyplot as plt
+import argparse
+
+
+parser = argparse.ArgumentParser(description="parse args")
+parser.add_argument('--resultdir', type=str)
+args = parser.parse_args()
 
 
 def threshold(alpha):
@@ -11,25 +16,38 @@ def threshold(alpha):
 def sample2beta(sample, mu, Sigma):
 
     Lambda = np.linalg.inv(Sigma)
-    beta2s = np.einsum('ij,jk,ik->i', sample-mu, Lambda, sample-mu)
+    beta2s = np.einsum('aj,ajk,ak->a', sample-mu, Lambda, sample-mu)
     return beta2s
 
 
-mu = [1.0, 1.0] 
-Sigma = [[3.0, 0.2],[0.2, 2.0]]
-norm = stats.multivariate_normal(mean=mu, cov=Sigma)
-sample = norm.rvs(size=10000)
+
+datadir = args.resultdir
+data = np.genfromtxt(datadir+'test_outputs_4.0.txt')
+N = data.shape[0]
+
+sample = np.empty((N,2))
+mu = np.empty((N, 2))
+Sigma = np.empty((N,2,2))
+
+for n in range(N):
+    mu[n] = data[n,0:2]
+    sample[n] = data[n,2:4]
+    Sigma[n,0,0] = data[n,4]
+    Sigma[n,1,1] = data[n,5]
+    Sigma[n,0,1] = data[n,6]
+    Sigma[n,1,0] = data[n,6]
+
 betas = sample2beta(sample, mu, Sigma)
 
 
 plist = []
-alist = np.arange(0.0, 1.0, 0.01)
+#alist = np.arange(0.0, 1.0, 0.01)
+alist = [0.1, 0.5, 0.9, 0.99]
 for alpha in alist:
 
     beta_th = threshold(alpha)
-    plist.append(np.sum(betas<beta_th)/10000)
+    plist.append(np.sum(betas<beta_th)/N)
 
-plt.figure()
-plt.plot(alist, alist, 'k')
-plt.plot(alist, plist, 'o', c='r', markersize=3)
-plt.show()
+
+print(plist)
+
