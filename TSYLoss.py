37f173966ad5f0824a34854fe74c11f899a0ea51
tak.yamamoto <tak.guruguru.4@gmail.com>
2022-03-07 20:01:39 +0900

    update 220307

--- TSYLoss.py
+++ TSYLoss.py
@@ -32,24 +32,20 @@ class CVAE_KLDiv(nn.Module):
         super(CVAE_KLDiv, self).__init__()
         
     def forward(self, mu1, logvar1, mu2, logvar2):
-
         var1 = logvar1.exp()
         var2 = logvar2.exp()
-        
         kl_div = var2/var1 + (mu1 - mu2)**2.0 / var1 + logvar1 - logvar2 - 1.0
-
         return torch.mean(kl_div.sum(dim=1) / 2.0)
 
 
 class CVAE_LogP(nn.Module):
-    def __init__(self):
+    def __init__(self, eps=1.0e-8):
         super(CVAE_LogP, self).__init__()
+        self.eps = eps
 
     def forward(self, mu, logvar, label):
-
         var = logvar.exp()
-        neg_logp = logvar + (mu - label)**2.0 / var + np.log(2.0*np.pi)
-        
+        neg_logp = logvar + (mu - label)**2.0 / (var + self.eps) + np.log(2.0*np.pi)
         return torch.mean( neg_logp.sum(dim=1) ) / 2.0
 
 
