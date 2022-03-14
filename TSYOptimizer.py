5702429ac701c007edb9ad6618b7fc0317e26b44
tak.yamamoto <tak.guruguru.4@gmail.com>
2022-03-14 11:53:38 +0900

    update 220314

--- TSYOptimizer.py
+++ TSYOptimizer.py
@@ -5,4 +5,9 @@ TSYOptimizer.py
 import torch.optim as optim
 
 def _TSYMakeOptimizer(netparameters, name, **kwargs):
-    return optim.__dict__[name](netparameters, **kwargs)
+
+    if name=="AdaBound":
+        import adabound
+        return adabound.AdaBound(netparameters, **kwargs)
+    else:
+        return optim.__dict__[name](netparameters, **kwargs)
